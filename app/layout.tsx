625301d655714cf0f9a779f0a7bdd5919b3d9dab
melo0725 <baway11100@gmail.com>
2025-05-23 14:06:50 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Coffee Shop',
+  description: 'Best coffee beans online',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
