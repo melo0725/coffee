fa28e80e72affc08ae96665c984e9d13d9d78926
melo0725 <baway11100@gmail.com>
2025-05-23 14:03:13 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-white text-black">
+      <h1 className="text-4xl font-bold mb-4">歡迎來到 Coffee Store</h1>
+      <p className="text-lg">我們提供精選咖啡豆，立即選購！</p>
+    </main>
+  );
+}
